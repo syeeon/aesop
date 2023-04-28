0f0e34226cfcdc191af3fe338859a631fb74465a
syeeon <glsy1150@naver.com>
2023-04-28 12:47:18 +0900

    230428 dbcon 수정

--- php/sub/inc/dbcon.php
+++ php/sub/inc/dbcon.php
@@ -1,6 +1,6 @@
 <?php
 $dbcon = mysqli_connect("localhost", "root", "", "aesop") or die("접속에 실패했습니다.");
 mysqli_set_charset($dbcon, "utf8");
-// $dbcon = mysqli_connect("localhost", "glsy1150", "d12th8wjd!", "glsy1150") or die("접속에 실패했습니다.");
+// $dbcon = mysqli_connect("localhost", "glsy1150", "d12th8wj**", "glsy1150") or die("접속에 실패했습니다.");
 // mysqli_set_charset($dbcon, "utf8");
 ?>
